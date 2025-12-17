5871e9c66a021f17960cf98abb9734ca0969a3fd
Artyom Yakovlev <maniakmill@gmail.com>
2025-12-17 18:20:52 +0200

    fix: fields for custom gift card route

--- backend/src/api/store/gift-cards/order/items/item/[id]/route.ts
+++ backend/src/api/store/gift-cards/order/items/item/[id]/route.ts
@@ -7,17 +7,31 @@ export async function GET(
 ) {
     const { id } = req.params;
 
+    const fields = req.query.fields?.split(",") ?? [
+        'id',
+        'status',
+        'value',
+        'code',
+        'currency_code',
+        'expires_at',
+        'reference_id',
+        'reference',
+        'line_item_id',
+        'note',
+        'metadata'
+    ];
+
     if (!id) {
         throw new MedusaError(
-            MedusaError.Types.INVALID_DATA,
-            "ID is required parametr."
+            MedusaError.Types.INVALID_ARGUMENT,
+            "ID is required."
         )
     }
 
     const query = req.scope.resolve("query")
     const { data: [gift_card], } = await query.graph({
         entity: "gift_cards",
-        fields: req.queryConfig.fields,
+        fields,
         filters: {
             line_item_id: id
         },
